c3b76c35a55956be17376e096a428966f58e6105
Ndreaass <ndreaass@gmail.com>
2021-12-28 09:30:10 +0700

    test 1

--- app/src/main/java/com/id/ac/budiluhur/notification/MainActivity.java
+++ app/src/main/java/com/id/ac/budiluhur/notification/MainActivity.java
@@ -1,10 +1,16 @@
 package com.id.ac.budiluhur.notification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     final String CHANNEL_ID = "contoh chanel";
@@ -13,12 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setView();
         createNotificationChannel();
-    }
-
-    private void setView() {
-
     }
 
     private void createNotificationChannel(){
@@ -27,7 +28,39 @@ public class MainActivity extends AppCompatActivity {
             CharSequence name = "Contoh Channel";
             String description = "Deskripsi Channel";
 
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,)
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,name,
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
+    }
+
+    public void doNotification(View view){
+        NotificationManagerCompat myManager = NotificationManagerCompat.from(this);
+
+        NotificationCompat.Builder myNotif = new NotificationCompat.Builder(this,CHANNEL_ID);
+        myNotif.setSmallIcon(android.R.drawable.ic_btn_speak_now);
+        myNotif.setContentTitle("1 Message Baru");
+        myNotif.setContentText("Hi, apa kabar?");
+        myNotif.setStyle(new NotificationCompat.BigTextStyle().bigText("Tersedia di Android 8.0 (API level 26) dan lebih tinggi.\u200B\n" +
+                                "\n" +
+                "set tingkat intrusi, seperti suara dan visibilitas untuk semua notifikasi yang di posting di saluran.\u200B\n" +
+                "\n" +
+                "Rentang dari IMPORTANCE_NONE(0) hingga IMPORTACE_HIGH(4).\u200B\n" +
+                "\n" +
+                "Untuk mendukung Versi Android sebelumnya (Lebih rendah dari API level 26), tetapkan prioritas (Notification priority).\u200B")
+        .setBigContentTitle("Title Big")
+        .setSummaryText("summary")
+        );
+
+        Intent i1 = new Intent(this,MainActivity.class);
+        PendingIntent pd = PendingIntent.getActivity(this,1,i1,0);
+        myNotif.setContentIntent(pd);
+        myNotif.setAutoCancel(true);
+
+        myManager.notify(1,myNotif.build());
+        finish();
     }
 }
